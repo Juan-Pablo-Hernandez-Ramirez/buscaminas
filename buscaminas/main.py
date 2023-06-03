591eef4f20aa505abcdaffb4773656670717ee29
Juan Hernandez <20301061550317@cetis155.edu.mx>
2023-06-03 12:06:55 -0600

    main screen

--- buscaminas/main.py
+++ buscaminas/main.py
@@ -1,1 +1,15 @@
-text
+# Package import
+from tkinter import *
+
+# Screen settings
+screen = Tk()
+screen.attributes("-fullscreen", True)
+screen.config(bg="gray90")
+
+# Buttons
+close_button = Button(screen, command=screen.destroy, width=5, text="x", bg="red", fg="white")
+close_button.place(x=1330, y=0)
+
+
+
+screen.mainloop()
